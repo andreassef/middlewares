d84de3ac43c203b2c33105e5f1b5eabd4623ec14
andreassef <andreassef02@gmail.com>
2021-03-29 00:21:30 -0400

    Implementacao de middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({error: "Usuário inexistente!"});
+  }
+
+  request.user = userExists;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next();
+  }
+
+  if (!user.pro && user.todos.length < 10) {
+    return next();
+  }
+
+  return response.status(403).json({error: "Para criar um TODO cadastre-se em nosso site!"});
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user  = users.find(user => user.username === username);
+  // const isUuid = '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$';
+  
+  if (!user) {
+    return response.status(404).json({error: "Usuário nao existe!"});
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({error: "invalid uuid!"});
+  }
+
+  const todo = user.todos.find(todos => todos.id === id);
+
+  if (!todo) {
+    return response.status(404).json({error: "Todo don't exists!"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if (!userExists) {
+    return response.status(404).json({error: "User don't exists"});
+  }
+
+  request.user = userExists;
+  return next();
 }
 
 app.post('/users', (request, response) => {
